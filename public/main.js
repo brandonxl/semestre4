88cb76aa3d572ca88320036ad1abfbec43b5fd70
Brandon Cortes <brasmiguel13@gmail.com>
2021-03-04 18:13:25 -0600

    diseno y codigo de la tarea

---
+++ public/main.js
@@ -0,0 +1,24 @@
+'use strict'
+
+function calculadora(){
+  let n1 =  parseFloat(document.getElementById('n1').value);
+  let n2 =   parseFloat(document.getElementById('n2').value);
+  let operaciones = document.getElementById('operaciones').value;
+
+    if(operaciones === '+'){
+        let sumar = document.getElementById('resultado').value = n1 + n2;
+    }
+    if(operaciones === '-'){
+        let restar = document.getElementById('resultado').value = n1 - n2;
+    }
+
+    if(operaciones === '/'){
+        let dividir = document.getElementById('resultado').value = n1 / n2;
+    }
+
+    if(operaciones === '*'){
+        let multiplicar = document.getElementById('resultado').value = n1 * n2;
+    }
+
+    
+}
